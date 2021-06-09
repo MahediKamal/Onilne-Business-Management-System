f41079745e248d9b3b9e2f8b3e3c72896ab3cbf4
Amrin36 <amrinrahman14@gmail.com>
2021-06-09 09:43:44 +0600

    designed main-homepage and admin-homepage

--- includes/header.php
+++ includes/header.php
@@ -7,7 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     
     <title>OBMS: <?php echo $title; ?></title>
 </head>
