08883a7797af20f9215d49006ea675db8388426b
Mahedi Kamal <mahedikamal44@gmail.com>
2021-06-11 16:09:12 +0600

    customer login information check and some design.

--- login_signup/customer_post_signup.php
+++ login_signup/customer_post_signup.php
@@ -5,16 +5,6 @@
     // Check if the form is submitted
     if ( isset( $_POST['submit'] ) ) {
 
-        // $CUSTOMER_ID = $_REQUEST['CUSTOMER_ID'];
-        // $CUSTOMER_NAME = $_REQUEST['CUSTOMER_NAME'];
-        // $CUSTOMER_CITY = $_REQUEST['CUSTOMER_CITY'];
-
-        // $supplier_id = $_REQUEST['supplier_id'];
-        // $supplier_name = $_REQUEST['supplier_name'];
-        // $supplier_company = $_REQUEST['supplier_company'];
-        // $supplier_phone = $_REQUEST['supplier_phone'];
-        // $supplier_country = $_REQUEST['supplier_country'];
-
 
         $cst_id = $_REQUEST['cst_id'];
         $cst_name = $_REQUEST['cst_name'];
@@ -31,17 +21,10 @@
         
 
 
-        // $sql = 'INSERT INTO customer(CUSTOMER_ID, CUSTOMER_NAME, CUSTOMER_CITY) '.
-        // 'VALUES(:id, :nme, :cty)';
-
-        // $sql = 'INSERT INTO supplier_info (supplier_id, supplier_name, supplier_company, supplier_phone, supplier_country)'. 
-        // 'VALUES (:id, :nme, :cmpny, :phn, :cnt)';
 
         $sql = 'INSERT INTO customer (cst_id, cst_name, cst_phone, cst_email, cst_city, cst_houseno, cst_street, cst_password )'.
         'VALUES (:id,:nme, :phn, :eml, :cty, :hus, :str,:pss)';
-        // $sql = 'INSERT INTO customer (cst_id, cst_name, cst_phone, cst_email, cst_city, cst_houseno, cst_street, cst_password )'.
-        // 'VALUES (9,' . "'".'sdf,'. "'". '1212' . "'".'dsf@sd,' . "'". 'sdf,'. "'". 'sdf,'. "'".'dsf,'. "'". '132'.')';
-
+      
         $stmt = oci_parse($con, $sql);
         
         oci_bind_by_name($stmt, ':id', $cst_id, -1);
@@ -55,22 +38,18 @@
         
         
         echo $sql;
-        // oci_bind_by_name($stmt, ':id', $CUSTOMER_ID);
-        // oci_bind_by_name($stmt, ':nme', $CUSTOMER_NAME);
-        // oci_bind_by_name($stmt, ':cty', $CUSTOMER_CITY);
+        
 
         $rc = oci_execute($stmt);
 
 
 
-        // $stmt = oci_parse($con,$isql);
-        // $rc=oci_execute($stmt);
-            if(!$rc){
-                $e = oci_error($stmt);
-                var_dump($e);
-            }
+        if(!$rc){
+            $e = oci_error($stmt);
+            var_dump($e);
+        }
 
-            oci_commit($con);
+        oci_commit($con);
 
         oci_free_statement($stmt);
         oci_close($con);
