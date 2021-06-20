2a8a040cb20f41e9b6561f37c8306fa7e3e472cb
Mahedi Kamal <mahedikamal44@gmail.com>
2021-06-20 20:23:48 +0600

    Designed place_order page.

--- customer/place_order.php
+++ customer/place_order.php
@@ -1,3 +1,69 @@
+<style>
+        .popup-container{
+            position: absolute;
+            left: 25%;
+            top: 10%;
+            z-index: 11111;
+            width:50%;
+            padding:20px;
+            border:1px solid #ccc;
+            box-shadow: 0px 0px 10px #ccc;
+            /* display: none; */
+            }
+        h2,h3,h4{
+            text-align: center;
+            margin-bottom: 0px;
+        }
+       .items{
+           width: 60%;
+           background-color: teal;
+           color: white;
+           display: flex;
+           height: 40px;
+           margin-left: 130px;
+           margin-top: 10px;
+           margin-bottom: 2px;
+           font-size: 18px;
+           font-family:sans-serif;
+           text-align: center;
+           padding-bottom: 10px;
+           padding-left: 50px;
+       }
+       .info p{
+           text-align: center;
+           font-size: 20px;
+           font-family: 'Times New Roman', Times, serif
+       }
+       .mail p{
+        text-align: center;
+           font-size: 20px;
+           font-family: 'Times New Roman', Times, serif
+       }
+        .close-btn{
+            float: right;
+            cursor: pointer;
+        }
+        
+        .btn{
+            background-color: teal;
+    color: white;
+    padding: 12px;
+    margin: 10px 0;
+    margin-left: 470px;
+    border: none;
+    width: 30%;
+    border-radius: 3px;
+    cursor: pointer;
+    font-size: 17px;
+    box-shadow: 0 0 1rem 0 rgba(99, 123, 150, 0.329);
+
+        }
+        .btn:hover{
+            background: white;
+            color: teal;
+            border: 1px solid teal;
+        }
+</style>
 <?php
     session_start();
     $cart_id = $_SESSION['cart_id'];
@@ -15,7 +81,7 @@
         $ac_num = $_REQUEST['ac_num'];
         $payment_type = $_REQUEST['payment_type'];
         $delivary = $_REQUEST['delivary'];
-        echo $ac_num . ' ' . $payment_type .' ' . $delivary .' '. $cst_id;
+        // echo $ac_num . ' ' . $payment_type .' ' . $delivary .' '. $cst_id;
         // echo "doe";
 
         //update billing_info, order_info and courier table
@@ -51,19 +117,6 @@
             var_dump($e);
         }
         //--------------order_info
-        /*
-        order_id  
-        order_date 
-        estimated_date
-        delivery_date 
-        freight_charge 
-        payment_status
-        total_discount 
-        order_cancel 
-        cst_id
-        courier_id 
-        */
-
         $sql = "UPDATE order_info
         SET order_date=sysdate ,estimated_date=sysdate+5 ,delivery_date=sysdate+5 ,freight_charge='1$' ,payment_status='due' ,total_discount= '1$',
         order_cancel='NO' 
@@ -78,21 +131,54 @@
             $e = oci_error($stmt);
             var_dump($e);
         }
-
-        oci_commit($con);
-
-
-
-        oci_free_statement($stmt);
-        oci_close($con);
-
-        echo "<h1>Congrats</h1>";
-        echo "<head>";
-            echo "<title>done</title>";
-            echo "<meta http-equiv = \"refresh\" content = \"5; url = ./index.php\" />";
-        echo "</head>";
-        exit;
+        // echo "<head>";
+        //     echo "<title>done</title>";
+        //     echo "<meta http-equiv = \"refresh\" content = \"5; url = ./index.php\" />";
+        // echo "</head>";
+        // exit;
 
     }
 
+?>
+<div class="popup-container">
+    <div class="close-btn">
+        <!-- <span class="close-btn">close</span> -->
+        <i class="fas fa-times"></i>
+    </div>
+    <h2>YOUR ORDER PLACED SUCCESSFULLY!</h2>
+    <?php
+     echo "<h3>ORDER ID: $order_id</h3>";
+    ?>
+  <div class="items">
+      <p>Items Purchased:
+       <?php Show_added_product_normally_av($con, $cart_id); ?>
+      </p>
+  </div>
+  <div class="info">
+    <?php
+        echo "<p> Name:$cst_name <br>";
+        echo "Email:$cst_email <br>";
+        //  Phone:01745587740 <br>
+        //  Address:Dhaka <br>
+        //  Amount to be paid:1000.00$ <br>
+        echo "Payment Mode:$payment_type ";
+        echo "</p>";
+    ?>
+  </div>
+
+    <div class="mail">
+        <p>Thank you!You will get mail from your chosen courier service soon.</p>
+    </div>
+
+    <div class="item">
+            <a href="../customer/index.php"> <button class="btn">Continue Shopping</button></a>
+               
+    </div>
+
+</div>
+
+<?php
+    oci_commit($con);
+    oci_free_statement($stmt);
+    oci_close($con);
 ?>
